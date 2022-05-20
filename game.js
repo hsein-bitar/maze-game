28764796cceb7f30bb615aff06fc3049d1064bc7
Hsein Bitar <hseinbitar22@gmail.com>
2022-05-20 12:11:49 +0300

    finished the maze game

--- game.js
+++ game.js
@@ -1,14 +1,56 @@
+
 window.onload = () => {
-    const start = document.getElementById('start');
     const status = document.getElementById('status');
+    const start = document.getElementById('start');
+    const boundaries = document.querySelectorAll('.boundary');
+    const scoreDiv = document.getElementsByClassName('example')[0];
+    const end = document.getElementById('end');
+    console.log(boundaries);
+
+    let score = 0
+
+    // event handlers
+    let startHandler = (e) => {
+        //reset maze state, no red boundaries,
+        boundaries.forEach(b => b.classList.remove('youlose'))
+        boundaries.forEach((b) => b.addEventListener('mouseover', failHandler));
+        //reset score
+        // score = 0;
+        scoreDiv.innerText = `Score: ${score}`
+        // reset game state
+        status.innerText = 'Begin by moving your mouse over the "S".';
+        end.addEventListener('mouseover', endHandler);
+    }
+
+    let stateHandler = (msg) => {
+        status.innerText = msg;
+    }
+
+    let failHandler = function (e) {
+        stateHandler('You Lost, try again');
+        e.target.classList.add('youlose');
+        score = score - 10;
+        scoreDiv.innerText = `Score: ${score}`
+        boundaries.forEach((b) => b.removeEventListener('mouseover', failHandler));
+        end.removeEventListener('mouseover', endHandler)
+        //remove end handler
+
+    }
+
+
+    let endHandler = () => {
+        stateHandler('You Won, Do it Again');
+        //remove boudaries handler
+        boundaries.forEach((b) => b.removeEventListener('mouseover', failHandler));
+        score = score + 5;
+        scoreDiv.innerText = `Score: ${score}`
+
+        end.removeEventListener('mouseover', endHandler)
+
+    }
     start.addEventListener('click', startHandler);
+    end.addEventListener('mouseover', endHandler)
+
 }
 
 
-// event handlers
-let startHandler = (e) => {
-    //reset maze state, no red boundaries,
-    //reset score
-    // reset game state
-    console.log(e.target.innerText);
-}
